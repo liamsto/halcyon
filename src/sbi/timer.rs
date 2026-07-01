a3cabdac36286a2ee4aba0d7195b4ce4bf8cf36b
Liam Storgaard <liam-git@aqrx.net>
2026-06-30 21:01:55 -0700

    style: remove unneeded `()` return type

--- src/sbi/timer.rs
+++ src/sbi/timer.rs
@@ -3,6 +3,6 @@ use crate::sbi::sbi_call;
 const EXTN_TIME: usize = 0x54494D45;
 const FID_SET_TIMER: usize = 0;
 
-pub fn set_timer(time: u64) -> () {
+pub fn set_timer(time: u64) {
     sbi_call(EXTN_TIME, FID_SET_TIMER, time as usize, 0, 0);
 }
